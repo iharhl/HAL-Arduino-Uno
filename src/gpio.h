dc01a4d0505512be7111f94816d8e46f3610a206
Ihar <ihluk@outlook.com>
2023-11-09 00:52:23 +0100

    Minor correction

--- src/gpio.h
+++ src/gpio.h
@@ -5,7 +5,7 @@
 #include "types.h"
 
 // -> TODO
-void _configurePin(volatile uint8_t* data_direction_register, uint8_t pin, IO_config_t setting);
+void _configureRegister(volatile uint8_t* data_direction_register, uint8_t pin, IO_config_t setting);
 
 void configurePin(uint8_t pin, IO_config_t setting);
 void digitalWrite(volatile uint8_t* port, uint8_t pin, digital_write_t value);
--- src/main.c
+++ src/main.c
@@ -11,6 +11,9 @@ int main(void)
 
     // Configure pin 6 (PD6) as output
     configurePin(6, OUTPUT);
+
+    // Test
+    configurePin(A0, OUTPUT);
     
     initPWM();
 
